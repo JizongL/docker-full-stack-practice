3ae0d8408053db39e4abd546c53b45366b7a45b4
tmm <blocklocmedia@gmail.com>
2019-09-26 22:32:44 -0400

    first commit

--- worker/index.js
+++ worker/index.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 
 const redisClient = redis.createClient({
 	host: keys.redisHost,
-	port: keys.redisHost,
+	port: keys.redisPort,
 	retry_strategy: () => 1000
 });
 const sub = redisClient.duplicate();
